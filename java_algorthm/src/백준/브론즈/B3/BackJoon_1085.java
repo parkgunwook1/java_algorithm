7fc23e16758e927e2c1d0e9a6ace3d59d86283bc
parkgunwook1 <pkw85428@naver.com>
2024-03-25 22:09:54 +0900

    백준 B3

---
+++ java_algorthm/src/백준/브론즈/B3/BackJoon_1085.java
@@ -0,0 +1,22 @@
+package 백준.브론즈.B3;
+
+import java.util.Scanner;
+
+public class BackJoon_1085  {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        int x_min = Math.min(x, w-x); // x축 최소거리
+        int y_min = Math.min(y, h-y); // y출 최소거리
+
+        // x와 y축 최소거리 중 가장 작은 값
+        System.out.println(Math.min(x_min, y_min));
+    }
+}

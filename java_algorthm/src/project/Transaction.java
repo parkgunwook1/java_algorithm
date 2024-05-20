ed4101fad3b3d9e7113c08748acdfebe06a5e0b3
parkgunwook1 <pkw85428@naver.com>
2024-05-20 22:49:01 +0900

    가계부 모듈

---
+++ java_algorthm/src/project/Transaction.java
@@ -0,0 +1,28 @@
+package project;
+
+/* 가계부 관리
+*
+* */
+
+// 수입 지출 항목 클래스
+public class Transaction {
+    private String date;
+    private String category;
+    private double amount;
+
+    public Transaction(String date, String category, double amount) {
+        this.date = date;
+        this.category = category;
+        this.amount = amount;
+    }
+    public String getDate() {
+        return date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    public double getAmount() {
+        return amount;
+    }
+}
